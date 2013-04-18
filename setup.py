e7f67df0478119f174d78dd0766b6b2333a5ed35
huseyinalb <huseyinalb@gmail.com>
2013-04-18 13:05:12 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='dedi',
+    version='0.0.1',
+    author=u'Huseyin Oguz Albayrak',
+    author_email='huseyinalb@gmail.com',
+    packages=['dedi'],
+    scripts=['bin/dedi'],
+    url='http://github.com/huseyinalb/dedi',
+    license='BSD licence',
+    description='A scheme like pet language project',
+    zip_safe=False,
+)
+
